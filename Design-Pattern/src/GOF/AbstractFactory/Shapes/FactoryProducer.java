707831d6d6367a1e502a9a2e149ed7ffbb46195f
Hamza Atif <hamza.atif.1121@gmail.com>
2023-10-26 03:51:05 +0500

    Update

---
+++ Design-Pattern/src/GOF/AbstractFactory/Shapes/FactoryProducer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.AbstractFactory.Shapes;
+
+/**
+ *
+ * @author fa20-bse-018
+ */
+public class FactoryProducer {
+   public static AbstractFactory getFactory(boolean rounded){   
+      if(rounded){
+         return new RoundedShapeFactory();         
+      }else{
+         return new ShapeFactory();
+      }
+   }
+}
+
