56232f3b266284472403fb5c6580f2354a188944
Hamza Atif <106954063+hamza-atif@users.noreply.github.com>
2023-12-14 08:38:34 +0500

    Interpreter

---
+++ Design-Pattern/src/GOF/Interpreter/ExpressionExample/Expression.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.Interpreter.ExpressionExample;
+
+/**
+ *
+ * @author fa20-bse-018
+ */
+public interface Expression {
+   public boolean interpret(String context);
+}
