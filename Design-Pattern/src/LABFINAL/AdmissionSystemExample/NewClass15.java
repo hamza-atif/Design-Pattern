b711b0e61337ef23c1a58e78f10da028f1881f6a
Hamza Atif <hamza.atif.1121@gmail.com>
2023-12-28 10:05:33 +0500

    Update

---
+++ Design-Pattern/src/LABFINAL/AdmissionSystemExample/NewClass15.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LABFINAL.AdmissionSystemExample;
+
+/**
+ *
+ * @author Hamza
+ */
+class DMCMarksEligibilityCheck implements EligibilityCheck {
+    public boolean checkEligibility(Student student) {
+        // DMCMarksEligibilityCheck logic for checking eligibility
+        return true;
+    }
+}
